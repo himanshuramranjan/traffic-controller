4a423e36bd1ae48ae67d3082cce7e749c082fb41
Himanshuram Ranjan <himanshuramranjan@Himanshurams-MacBook-Air.local>
2025-07-20 12:04:43 +0530

    demo added for the traffic controller

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,19 @@
-import models.TrafficController;
+import service.TrafficController;
 import models.TrafficLight;
 
 public class Main {
     public static void main(String[] args) {
-
-        TrafficLight trafficLight = new TrafficLight(1, 10, 10, 10);
+        TrafficLight trafficLight = new TrafficLight(1, 2000, 2000, 2000);
         TrafficController trafficController = TrafficController.getInstance(trafficLight);
 
         trafficController.startTrafficControlSystem();
+
+        // Simulate emergency after a few seconds
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            System.out.println("Exception occurred : " + e);
+        }
 
         trafficController.handleEmergency();
     }
